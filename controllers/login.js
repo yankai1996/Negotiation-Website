eaeca68a64fb51d2b65b462c8941fbc4f596df77
Kai Yan <u3514123@connect.hku.hk>
2018-06-28 00:02:12 +0800

    authorization

--- controllers/login.js
+++ controllers/login.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var getRouter = express.Router();
-var postRouter = express.Router();
-
+const express = require('express');
+const getRouter = express.Router();
+const postRouter = express.Router();
+const System = require('../models/system');
 
 getRouter.get('/', function(req, res){
     res.redirect('/login');
@@ -11,17 +11,40 @@ getRouter.get('/login', function(req, res, next){
     res.render('login', {flag: 0});
 });
 
-postRouter.post('/login', function(req, res){
-    if (req.body.username == 'hello' && req.body.password == 'world') {
-        res.cookie('authorized', req.body.username);
+function authInstructor(req, res, next){
+    let username = req.body.username,
+        password = req.body.password;
+    if (System.verifyInstructor(username, password)) {
+        res.cookie('instructor', username);
         res.redirect('/admin');
-    } else if (req.body.username == 'hi') {
-        res.cookie('loggedIn', req.body.username);
-        res.redirect('/welcome');
     } else {
-        res.render('login', {flag: 1});
+        next();
     }
-});
+}
+
+function authParticipant(req, res, next){
+    let username = req.body.username,
+        password = req.body.password;
+    System.verifyParticipant(username, password).then(function(verified){
+        if (verified) {
+            res.cookie('participant', username);
+            res.redirect('/welcome');
+        } else {
+            next()
+        }
+    }).catch(function(error){
+        console.log(error);
+        next()
+    });
+}
+
+postRouter.post('/login', 
+    authInstructor, 
+    authParticipant,
+    function(req, res){
+        res.render('login', {flag: 1});
+    }   
+);
 
 exports.get = getRouter;
 exports.post = postRouter;

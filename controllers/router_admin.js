1e13fa97fd685d6ed8ebf2a4ff5c4c789113fa27
Kai Yan <u3514123@connect.hku.hk>
2018-07-05 16:47:57 +0800

    check input safer

--- controllers/router_admin.js
+++ controllers/router_admin.js
@@ -1,46 +1,46 @@
-const express = require('express');
-const getRouter = express.Router();
-const postRouter = express.Router();
-const auth = require('./auth')
+var express = require('express');
+var getRouter = express.Router();
+var postRouter = express.Router();
+var auth = require('./auth')
 
-const Instructor = require('../models/instructor');
-const System = require('../models/system');
+var Instructor = require('../models/instructor');
+var System = require('../models/system');
 
 
 // add games to request
-function getGames(req, res, next){
-    Instructor.getGames().then(function(games){
+const getGames = (req, res, next) => {
+    Instructor.getGames().then((games) => {
         req.games = games;
         next();
-    }).catch(function(err){
+    }).catch((err) => {
         req.err = err;
         sendError(req, res);
     });
 }
 
 // add participants to request
-function getParticipants(req, res, next){
-    Instructor.getPairedParticipants().then(function(participants){
+const getParticipants = (req, res, next) => {
+    Instructor.getPairedParticipants().then((participants) => {
         req.participants = participants;
         next();
-    }).catch(function(err){
+    }).catch((err) => {
         req.err = err;
         sendError(req, res);
     });
 }
 
-function countParticipants(req, res, next){
-    Instructor.countParticipants().then(function(count){
+const countParticipants = (req, res, next) => {
+    Instructor.countParticipants().then((count) => {
         req.count = count;
         next();
-    }).catch(function(err){
+    }).catch((err) => {
         req.err = err;
         sendError(req, res);
     });
 }
 
 // render the admin page
-function renderAdmin(req, res, next){
+const renderAdmin = (req, res, next) => {
     try {
         res.render('admin', {
             games: req.games,
@@ -54,7 +54,7 @@ function renderAdmin(req, res, next){
 }
 
 // send error message
-function sendError(req, res){
+const sendError = (req, res) => {
     console.log(req.err);
     res.send("ERROR: " + req.err)
 }
@@ -68,21 +68,35 @@ getRouter.get('/admin', sendError);
 
 
 // format and check the received data of games
-function checkGameData(req, res, next){
+const checkGameData = (req, res, next) => {
     var game = System.parseGame(req.body);
     for (var i in game){
         if (isNaN(game[i])){
-            res.send({
-                errMsg: "Input " + i + " is NaN!"
-            });
+            res.send("Input " + i + " is NaN!");
+            return;
         } 
+        if (i == 'alpha' || i == 'beta' || i == 'gamma') {
+            if (game[i] < 0 || game[i] > 1) {
+                res.send("Input " + i + " should be 0 ~ 1.");
+                return;
+            }
+        } else if (game.t < 1 || game.t > 99) {
+            res.send("Input T should be 1 ~ 99.");
+            return;
+        } else if (game.w < 0 || game.w >= 10000) {
+            res.send("Input T should be 0 ~ 9999.");
+            return;
+        } else if (game.n > 1000) {
+            res.send("Please try a smaller n.");
+            return;
+        }
     }
     req.game = game;
     next();
 }
 
 // add games and send the game data
-function addGames(req, res, next){
+const addGames = (req, res, next) => {
     Instructor.addGames(req.game).then((result) => {
         next();
     }).catch((err) => {
@@ -111,13 +125,13 @@ postRouter.post('/admin/add_games', sendError);
 
 
 // delete games and send the game data
-function deleteGames(req, res, next){
-    Instructor.deleteGames(req.game).then(function(result){
+const deleteGames = (req, res, next) => {
+    Instructor.deleteGames(req.game).then((result) => {
         res.send({
             success: 1,
             game: req.game
         });
-    }).catch(function(err){
+    }).catch((err) => {
         req.err = err;
         next();
     });
@@ -129,7 +143,7 @@ postRouter.post('/admin/delete_games', sendError);
 
 
 // check if valid number of participants to be added
-function checkNumber(req, res, next){
+const checkNumber = (req, res, next) => {
     var number = parseInt(req.body.number);
     if (number <= 0 || number > 100) {
         res.send("Please enter an number in 1~100!");
@@ -139,17 +153,17 @@ function checkNumber(req, res, next){
 }
 
 // add participants and send the number that has been added
-function addParticipants(req, res, next){
-    Instructor.addParticipants(req.number).then(function(result){
+const addParticipants = (req, res, next) => {
+    Instructor.addParticipants(req.number).then((result) => {
         next()
-    }).catch(function(err){
+    }).catch((err) => {
         req.err = err;
         sendError(req, res);
     })
 }
 
 // send the updated participant table
-function sendUpdatedParticipants(req, res, next){
+const sendUpdatedParticipants = (req, res, next) => {
     try {
         res.send({
             success: 1,
@@ -171,14 +185,14 @@ postRouter.post('/admin/add_participants', sendError);
 
 
 // get and send games according to participant id
-function viewPair(req, res, next){
+const viewPair = (req, res, next) => {
     // console.log(req.body.id);
-    Instructor.getGamesByParticipant(req.body.id).then(function(result){
+    Instructor.getGamesByParticipant(req.body.id).then((result) => {
         res.send({
             success: 1,
             games: result
         });
-    }).catch(function(err){
+    }).catch((err) => {
         req.err = err;
         next();
     });
@@ -189,12 +203,12 @@ postRouter.post('/admin/view_pair', sendError);
 
 
 // remove game from pair
-function removeGame(req, res, next){
-    Instructor.removePairFromGame(req.body.gameId).then(function(result){
+const removeGame = (req, res, next) => {
+    Instructor.removePairFromGame(req.body.gameId).then((result) => {
         // pass request to viewPair()
         req.body.id = req.body.buyerId
         next();
-    }).catch(function(err){
+    }).catch((err) => {
         req.err = err;
         sendError(req, res);
     });
@@ -205,13 +219,13 @@ postRouter.post('/admin/remove_game', viewPair);
 postRouter.post('/admin/remove_game', sendError);
 
 
-function getAvalibaleGames(req, res, next){
-    Instructor.getAvailableGames().then(function(games){
+const getAvalibaleGames = (req, res, next) => {
+    Instructor.getAvailableGames().then((games) => {
         res.send({
             success: 1,
             games: games
         });
-    }).catch(function(err){
+    }).catch((err) => {
         req.err = err;
         next();
     });
@@ -221,7 +235,7 @@ postRouter.post('/admin/get_available_games', getAvalibaleGames);
 postRouter.post('/admin/get_available_games', sendError);
 
 
-function assignGamesToPair(req, res, next){
+const assignGamesToPair = (req, res, next) => {
     var games = JSON.parse(req.body.gamesString);
     Instructor.assignGamesToPair(games).then(games => {
         res.send({

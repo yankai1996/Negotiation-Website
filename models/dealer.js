392b307e89041823e74e603270c5ed62934ac043
Kai Yan <u3514123@connect.hku.hk>
2018-07-10 15:13:36 +0800

    initial commit game logic

--- models/dealer.js
+++ models/dealer.js
@@ -1,7 +1,49 @@
 const db = require('./db');
+const MasterGame = db.MasterGame;
+const Game = db.Game;
+const Participant = db.Participant;
+const EVENT = require('../controllers/socket').EVENT;
 
-var Game = db.Game;
-var Participant = db.Participant;
+
+function Dealer(first, second, io) {
+    this.first = first;
+    this.second = second;
+    this.io = io;
+
+    var buyer, seller;
+
+    this.warmup = async () => {
+        var masterGame = await MasterGame.findOne({
+            where: {is_warmup: true},
+            raw: true
+        });
+        var game = await Game.findOne({
+            where: {
+                master_game: masterGame.id,
+                is_done: false,
+                $or: [{buyer_id: this.first},
+                    {seller_id: this.first}]
+            },
+            raw: true
+        });
+        buyer = game.buyer_id;
+        seller = game.seller_id;
+    }
+
+    this.period = () => {
+
+    }
+
+    this.timeout = () => {
+
+    }
+
+    this.newGame = () => {
+
+    }
+}
+exports.Dealer = Dealer;
+
 
 exports.getOpponent = (id) => {
     return Participant.findOne({
